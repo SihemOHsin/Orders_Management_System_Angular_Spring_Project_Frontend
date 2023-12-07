7f714459226424e77cb6491a35d9b8f4f3f482e7
sihem hsin <sihemouledhsin@gmail.com>
2023-12-07 23:33:20 +0100

    first commit

---
+++ src/app/services/bill.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BillService {
+  url=environment.apiURL;
+  jsonHeader = {
+   headers: new HttpHeaders().set('Content-Type', 'application/json'),
+ };
+
+ 
+  constructor( private http: HttpClient
+    ) { }
+
+    generateReport(data: any){
+      return this.http.post(`${this.url}/bill/generateReport`,data,this.jsonHeader);
+    }
+    getPdf(data: any): Observable<Blob>{
+      return this.http.post(this.url+'/bill/getPdf',data,{responseType: 'blob'});
+    }
+    
+    getBills(){
+      return this.http.get(this.url + "/bill/getBills");
+      //return this.http.get(this.url+'/bill/getBills',this.jsonHeader);
+    }
+    delete(id:any){
+
+      return this.http.post(this.url+'/bill/delete/'+id,this.jsonHeader);
+
+    }
+
+}
